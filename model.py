ff4d361c8708ede98234f2b36d722b579b6fe345
mawanda-jun <giovanni.cavallin@outlook.com>
2020-05-02 19:30:35 +0200

    created code for submission

--- model.py
+++ model.py
@@ -1,11 +1,15 @@
-from network import ShallowNet, train_batch, val_batch
+from network import ShallowNet, SmartDense3D
 from pytorchtools import EarlyStopping, TReNDSMetrics
 
+import os
 import torch
 import torch.nn as nn
+import pandas as pd
+from tqdm import tqdm
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from typing import Dict, Union
+import gc
 
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -13,13 +17,13 @@ DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 class Model:
     def __init__(self,
                  net_type: str,
-                 net_params: Dict[str, Union[str, int]],
+                 net_hyperparams: Dict[str, Union[str, int, float]],
                  optimizer: str = 'adam',
-                 loss: nn.Module = 'humber', # SmoothL1Loss
+                 loss: str = 'humber',  # SmoothL1Loss
                  lr: float = .01,
                  ):
         self.net_type = net_type
-        self.net_params = net_params
+        self.net_hyperparams = net_hyperparams
         self.optimizer = optimizer
         self.loss = loss
         self.lr = lr
@@ -28,17 +32,15 @@ class Model:
 
     def __build_model(self) -> (nn.Module, nn.Module, torch.optim, nn.Module):
         # Mandatory parameters to be used.
-        dropout_prob = self.net_params['dropout_prob']
-        fnc_dim = self.net_params['fnc_dim']
-        sbm_dim = self.net_params['sbm_dim']
+        dropout_prob = self.net_hyperparams['dropout_prob']
 
         # Define custom network. In each one the specific parameters must be added from self.net_params
         if self.net_type == 'ShallowNet':
-            network: nn.Module = ShallowNet(fnc_dim, sbm_dim, dropout_prob)
+            network: nn.Module = ShallowNet(dropout_prob)
+        elif self.net_type == 'SmartDense3D':
+            network: nn.Module = SmartDense3D(dropout_prob)
         else:
             raise ValueError("Bad network type. Please choose ShallowNet or ...")
-
-        network.to(DEVICE)  # Send network module to DEVICE
 
         # Define metric, loss, optimizer
         metric_fn = TReNDSMetrics()  # Define metric function
@@ -47,18 +49,31 @@ class Model:
             loss_fn = nn.SmoothL1Loss()  # Define loss function
         elif self.loss == 'metric':
             loss_fn = TReNDSMetrics()
+        elif self.loss == 'MAE':
+            loss_fn = nn.L1Loss()
+        elif self.loss == 'MSE':
+            loss_fn = nn.MSELoss()
         else:
             raise ValueError("Bad loss type. Please choose humber or...")
 
         if self.optimizer == 'adam':
             # Define the optimizer. It wants to know which parameters are being optimized.
             optimizer_fn = torch.optim.Adam(params=network.parameters(), lr=self.lr, weight_decay=1e-5)
+        elif self.optimizer == 'SGD':
+            optimizer_fn = torch.optim.SGD(params=network.parameters(), lr=self.lr, momentum=0.3, weight_decay=1e-7)
         else:
             raise ValueError('Bad optimizer type. Please choose adam or ...')
 
         return metric_fn, loss_fn, optimizer_fn, network
 
-    def fit(self, epochs, train_loader, val_loader, patience, run_name = None):
+    def __save(self, run_path, metric, epoch):
+        state = {
+            'state_dict': self.net.state_dict()
+        }
+        filepath = os.path.join(run_path, 'checkpoint_' + str(metric) + '_ep' + str(epoch) + '.pt')
+        torch.save(state, filepath)
+
+    def fit(self, epochs, train_loader, val_loader, patience, run_path=None):
         early_stopping = EarlyStopping(patience=patience, verbose=False)
 
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer_fn, len(train_loader), 1e-7)
@@ -73,37 +88,60 @@ class Model:
         for epoch in range(epochs):
             start_epoch.record()
 
-            train_loss, train_metric = train_batch(self.net, train_loader, self.loss_fn, self.metric_fn, self.optimizer_fn, DEVICE)
-            val_loss, val_metric = val_batch(self.net, val_loader, self.loss_fn, self.metric_fn, DEVICE)
+            train_loss, train_metric = self.net.train_batch(self.net, train_loader, self.loss_fn, self.metric_fn, self.optimizer_fn, DEVICE)
+            val_loss, val_metric = self.net.val_batch(self.net, val_loader, self.loss_fn, self.metric_fn, DEVICE)
 
             end_epoch.record()
             torch.cuda.synchronize(DEVICE)
+            # Calculate elapsed time
+            elapsed_seconds = start_epoch.elapsed_time(
+                        end_epoch) / 1000
+            elapsed_minutes = elapsed_seconds // 60
+            elapsed_seconds = elapsed_seconds % 60
             print(
-                "Epoch: {}\ttrain: acc: {:.4f} loss: {:.4f}\t\tval: acc: {:.4f} loss: {:.4f}\ttime: {:.4}s".format(
-                    epoch,
+                "\nEpoch: {}\ttrain metric: {:.4f} loss: {:.4f}\t\tval metric: {:.4f} loss: {:.4f}\ttime: {:.4}m{:.4}s".format(
+                    epoch+1,
                     train_metric,
                     train_loss,
                     val_metric,
                     val_loss,
-                    start_epoch.elapsed_time(
-                        end_epoch) / 1000))
+                    elapsed_minutes,
+                    elapsed_seconds
+                ))
             # Update early stopping. This is really useful to stop training in time.
             # The if statement is not slowing down training since each epoch last very long.
-            # PLEASE TAKE NOTE THAT we are using epoch_val_acc, since it brings the score function of the competition
-            float_epoch_val_acc = val_loss.detach().cpu().numpy()
-            float_epoch_train_acc = train_loss.detach().cpu().numpy()
-            early_stopping(float_epoch_train_acc, float_epoch_val_acc, self.net)
-            # if early_stopping.save_checkpoint and run_name:
-            #     self.save(run_name, float_epoch_val_acc, epoch)
-            # if early_stopping.early_stop:
-            #     print("Early stopping")
-            #     break
+            epoch_val_metric = val_metric.item()
+            epoch_train_metric = train_metric.item()
+            early_stopping(epoch_train_metric, epoch_val_metric, self.net)
+            if early_stopping.save_checkpoint and run_path:
+                self.__save(run_path, epoch_val_metric, epoch)
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
 
             # Update scheduler
             scheduler.step()
         end_whole.record()
         torch.cuda.synchronize(DEVICE)
-        print("Elapsed time: {:.4f}s".format(start_whole.elapsed_time(end_whole) / 1000))
+        print("Elapsed time: {:.4f}m".format(start_whole.elapsed_time(end_whole) / 60000))
 
-        # Return val_loss_min for KFold - which is, the metric that we register for early stopping.
-        return early_stopping.val_loss_min
+        # Return the best metric that we register for early stopping.
+        return early_stopping.val_metric_min
+
+    def submit(self, test_loader, run_path):
+        print('Predicting test set...')
+        IDs, outputs = self.net.predict_batch(self.net, test_loader, DEVICE)
+        submission = pd.DataFrame(columns=['Id', 'Predicted'])
+        sub_names = [
+            '_age',
+            '_domain1_var1',
+            '_domain1_var2',
+            '_domain2_var1',
+            '_domain2_var2'
+        ]
+
+        for ID, output in tqdm(zip(IDs, outputs), desc='Writing predictions on submission.csv file...', total=len(outputs)):
+            sub_names_part = [str(int(ID)) + sn for sn in sub_names]
+            for name, out in zip(sub_names_part, output):
+                submission.loc[len(submission['Id'])] = [name, out]
+        submission.to_csv(os.path.join(run_path, 'submission.csv'), index=False)
--- train.py
+++ train.py
@@ -1,14 +1,30 @@
 from model import Model
 from dataset import TReNDS_dataset, ToTensor
-
+import shutil
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
 import os
 
+
+def clean_folder(folder, metric, delta=0.02):
+    """
+    Cleans all checkpoints that are distant > delta from average metric
+    :param folder: folder path
+    :return: None
+    """
+    for file in os.listdir(folder):
+        if 'checkpoint' in file:
+            filename = file.split('.')[0] + '.' + file.split('.')[1]
+            metric_value = filename.split('_')[1]
+            metric_value = float(metric_value)
+            if not metric - delta < metric_value < metric + delta:
+                os.remove(os.path.join(folder, file))
+
+
 if __name__ == '__main__':
     # Define paths
     base_path = '..'
-    mat_folder = os.path.join(base_path, 'dataset/Kaggle/fMRI_train')
+    train_pth_folder = os.path.join(base_path, 'dataset/Kaggle/fMRI_train_torch')
     fnc_path = os.path.join(base_path, 'dataset/Kaggle/fnc.csv')
     sbm_path = os.path.join(base_path, 'dataset/Kaggle/loading.csv')
     ICN_num_path = os.path.join(base_path, 'dataset/Kaggle/ICN_numbers.csv')
@@ -19,29 +35,58 @@ if __name__ == '__main__':
     trans = transforms.Compose([ToTensor()])
 
     # Create dataset
-    dataset = TReNDS_dataset(mat_folder, fnc_path, sbm_path, ICN_num_path, train_scores_path, mask_path, trans)
+    dataset = TReNDS_dataset(train_pth_folder, fnc_path, sbm_path, ICN_num_path, train_scores_path, mask_path, trans)
 
     # Split dataset in train/val
     val_dim = 0.3
     dataset_len = len(dataset)
     train_len = round(dataset_len * (1-val_dim))
     val_len = round(dataset_len * val_dim)
+
     train_set, val_set = random_split(dataset, [train_len, val_len])
 
-    # Define train and val loaders
-    train_loader = DataLoader(train_set, batch_size=512, shuffle=True, pin_memory=True)
-    val_loader = DataLoader(val_set, batch_size=val_len, shuffle=False, pin_memory=True)
+    # Define training hyper parameters
+    network_type = 'SmartDense3D'
+    optimizer = 'adam'
+    loss = 'metric'
+    learning_rate = 5e-5
+    batch_size = 32
+    dropout_prob = 0.3
+    patience = 5
 
-    # Define net params
-    net_params = {
-        'fnc_dim': 1378,
-        'sbm_dim': 26,
-        'dropout_prob': 0.3
+    # Define network hyper params
+    net_hyperparams = {
+        'dropout_prob': dropout_prob
     }
 
+    # Define train and val loaders
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=4)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=4)
+
     # Define model
-    model = Model('ShallowNet', net_params, 'adam', 'metric', lr=1e-4)
+    model = Model(network_type, net_hyperparams, optimizer, loss, lr=learning_rate)
+
+    run_path = os.path.join('experiments',
+                            network_type +
+                            '_loss.' + loss +
+                            '_batchsize.' + str(batch_size) +
+                            '_optimizer.' + optimizer +
+                            '_lr.' + str(learning_rate) +
+                            '_drop.' + str(dropout_prob) +
+                            '_patience.' + str(patience) +
+                            '_other_net.' + str(0))
+    os.makedirs(run_path, exist_ok=False)
 
     # Train model
-    model.fit(5000, train_loader, val_loader, 10)
+    val_metric = model.fit(15000, train_loader, val_loader, patience, run_path)
+    # Clean checkpoint folder from all the checkpoints that are useless
+    clean_folder(run_path, val_metric, delta=0.002)
+
+    # Make backup of network and model files into run folder
+    shutil.copy('network.py', run_path)
+    shutil.copy('model.py', run_path)
+    shutil.copy('train.py', run_path)
+    shutil.copy('dataset.py', run_path)
+    shutil.copy('pytorchtools.py', run_path)
+
 
