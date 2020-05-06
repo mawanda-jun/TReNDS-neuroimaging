7b117ab6f0fc7ef3a1633a604fc8303ece660846
mawanda-jun <giovanni.cavallin@outlook.com>
2020-05-06 10:36:14 +0200

    new hparameters and folders

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 from model import Model
-from dataset import TReNDS_dataset, ToTensor
+from dataset import TReNDS_dataset, ToTensor, Normalize, AugmentDataset, fMRI_Aumentation
 import shutil
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
 import os
+from torchsummary import summary
 
 
 def clean_folder(folder, metric, delta=0.02):
@@ -24,18 +25,22 @@ def clean_folder(folder, metric, delta=0.02):
 if __name__ == '__main__':
     # Define paths
     base_path = '..'
-    train_pth_folder = os.path.join(base_path, 'dataset/Kaggle/fMRI_train_torch')
-    fnc_path = os.path.join(base_path, 'dataset/Kaggle/fnc.csv')
+    train_torch_folder = os.path.join(base_path, 'dataset/fMRI_train_torch')
+    # fnc_path = os.path.join(base_path, 'dataset/Kaggle/fnc.csv')
     sbm_path = os.path.join(base_path, 'dataset/Kaggle/loading.csv')
-    ICN_num_path = os.path.join(base_path, 'dataset/Kaggle/ICN_numbers.csv')
+    # ICN_num_path = os.path.join(base_path, 'dataset/Kaggle/ICN_numbers.csv')
     train_scores_path = os.path.join(base_path, 'dataset/Kaggle/train_scores.csv')
-    mask_path = os.path.join(base_path, 'dataset/Kaggle/fMRI_mask.nii')
+    # mask_path = os.path.join(base_path, 'dataset/Kaggle/fMRI_mask.nii')
 
-    # Define transformations for files
-    trans = transforms.Compose([ToTensor()])
+    # No need to normalize train set since it has already been normalized while transformed
+    # mean_path = os.path.join(base_path, 'dataset', 'mean.pt')
+    # variance_path = os.path.join(base_path, 'dataset', 'variance.pt')
+
+    # Define transformations
+    common_trans = transforms.Compose([ToTensor()])
 
     # Create dataset
-    dataset = TReNDS_dataset(train_pth_folder, fnc_path, sbm_path, ICN_num_path, train_scores_path, mask_path, trans)
+    dataset = TReNDS_dataset(train_torch_folder, sbm_path, train_scores_path=train_scores_path, transform=common_trans)
 
     # Split dataset in train/val
     val_dim = 0.3
@@ -45,14 +50,18 @@ if __name__ == '__main__':
 
     train_set, val_set = random_split(dataset, [train_len, val_len])
 
+    # Define augmentations for training dataset
+    # TODO: add augmentation to make the training better. However, the model search will be without because not feasible
+    train_set = AugmentDataset(train_set, fMRI_Aumentation())
+
     # Define training hyper parameters
-    network_type = 'SmartDense3D'
+    network_type = 'CustomResNet3D50'
     optimizer = 'adam'
     loss = 'metric'
-    learning_rate = 5e-5
-    batch_size = 32
-    dropout_prob = 0.3
-    patience = 10
+    learning_rate = 5e-6
+    batch_size = 16
+    dropout_prob = 0.5
+    patience = 20
 
     # Define network hyper params
     net_hyperparams = {
@@ -60,11 +69,14 @@ if __name__ == '__main__':
     }
 
     # Define train and val loaders
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=6)
-    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=6)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=9)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=2)
 
     # Define model
     model = Model(network_type, net_hyperparams, optimizer, loss, lr=learning_rate)
+
+    # Print model network
+    # summary(model.net, {'sbm': '10001', 'brain': (53, 52, 63, 53)})
 
     run_path = os.path.join('experiments',
                             network_type +
@@ -74,7 +86,7 @@ if __name__ == '__main__':
                             '_lr.' + str(learning_rate) +
                             '_drop.' + str(dropout_prob) +
                             '_patience.' + str(patience) +
-                            '_other_net.' + str(1))
+                            '_other_net.' + 'resnet50')
     os.makedirs(run_path, exist_ok=False)
 
     # Train model
@@ -88,5 +100,7 @@ if __name__ == '__main__':
     shutil.copy('train.py', run_path)
     shutil.copy('dataset.py', run_path)
     shutil.copy('pytorchtools.py', run_path)
+    shutil.copy('DenseNet3D.py', run_path)
+    shutil.copy('ResNet.py', run_path)
 
 
