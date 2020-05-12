1adb7aad7fc827b37f20c9001b472d568069eaa0
mawanda-jun <giovanni.cavallin@outlook.com>
2020-05-12 11:52:09 +0200

    added information for lr range test. Added new options for training

--- train.py
+++ train.py
@@ -1,10 +1,12 @@
 from model import Model
-from dataset import TReNDS_dataset, ToTensor, Normalize, AugmentDataset, fMRI_Aumentation
+from dataset import TReNDS_dataset, ToTensor, AugmentDataset, fMRI_Aumentation
 import shutil
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
 import os
-from pytorch_model_summary import summary
+
+# from torch_lr_finder import LRFinder
+# import json
 
 
 def clean_folder(folder, metric, delta=0.02):
@@ -25,7 +27,7 @@ def clean_folder(folder, metric, delta=0.02):
 if __name__ == '__main__':
     # Define paths
     base_path = '..'
-    train_torch_folder = os.path.join(base_path, 'dataset/fMRI_train_torch')
+    train_torch_folder = os.path.join(base_path, 'dataset/fMRI_train_norm')
     # fnc_path = os.path.join(base_path, 'dataset/Kaggle/fnc.csv')
     sbm_path = os.path.join(base_path, 'dataset/Kaggle/loading.csv')
     # ICN_num_path = os.path.join(base_path, 'dataset/Kaggle/ICN_numbers.csv')
@@ -36,11 +38,8 @@ if __name__ == '__main__':
     # mean_path = os.path.join(base_path, 'dataset', 'mean.pt')
     # variance_path = os.path.join(base_path, 'dataset', 'variance.pt')
 
-    # Define transformations
-    common_trans = transforms.Compose([ToTensor()])
-
     # Create dataset
-    dataset = TReNDS_dataset(train_torch_folder, sbm_path, train_scores_path=train_scores_path, transform=common_trans)
+    dataset = TReNDS_dataset(train_torch_folder, sbm_path, train_scores_path=train_scores_path)
 
     # Split dataset in train/val
     val_dim = 0.3
@@ -50,18 +49,22 @@ if __name__ == '__main__':
 
     train_set, val_set = random_split(dataset, [train_len, val_len])
 
-    # Define augmentations for training dataset
-    # TODO: add augmentation to make the training better. However, the model search will be without because not feasible
-    train_set = AugmentDataset(train_set, fMRI_Aumentation())
+    # Define transformations
+    train_trans = transforms.Compose([fMRI_Aumentation(), ToTensor(use_fnc=False, train=True)])
+    val_trans = ToTensor(use_fnc=False, train=True)
+
+    train_set = AugmentDataset(train_set, train_trans)
+    val_set = AugmentDataset(val_set, val_trans)
 
     # Define training hyper parameters
-    network_type = 'PlainDenseNet3D'
-    optimizer = 'adam'
+    network_type = 'PlainResNet3D10'
+    optimizer = 'adamw'
     loss = 'metric'
-    learning_rate = 1e-5
-    batch_size = 7
+    learning_rate = 4e-3
+    learning_rate_decay = 1.
+    batch_size = 32
     dropout_prob = 0.4
-    patience = 20
+    patience = 10
     num_init_features = 128
 
     # Define network hyper params
@@ -71,40 +74,49 @@ if __name__ == '__main__':
     }
 
     # Define train and val loaders
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=2)
-    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=2)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=12)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=4)
 
     # Define model
-    model = Model(network_type, net_hyperparams, optimizer, loss, lr=learning_rate)
+    model = Model(network_type, net_hyperparams, optimizer, loss, lr=learning_rate, lr_decay=learning_rate_decay)
 
-    # Print model network
-    # summary(model.net, {'sbm': '10001', 'brain': (53, 52, 63, 53)})
+    # network = model.net
+    # criterion = model.loss
+    # optimizer = model.optimizer
+    # lr_finder = LRFinder(network, optimizer, criterion, device='cuda:0')
+    # lr_finder.range_test(train_loader, val_loader, end_lr=10., num_iter=100)
+    # json.dump(lr_finder.history, open('lr_finder.json', 'w'))
+    # lr_finder.plot()
+    # lr_finder.reset()
 
     run_path = os.path.join('experiments',
                             network_type +
-                            '_loss.' + loss +
-                            '_batchsize.' + str(batch_size) +
-                            '_optimizer.' + optimizer +
-                            '_lr.' + str(learning_rate) +
-                            '_drop.' + str(dropout_prob) +
-                            '_patience.' + str(patience) +
                             '_numInitFeatures.' + str(num_init_features) +
-                            '_other_net.' + str(0))
+                            '_lr.' + str(learning_rate) +
+                            '_lr_decay.' + str(learning_rate_decay) +
+                            '_drop.' + str(dropout_prob) +
+                            '_batchsize.' + str(batch_size) +
+                            '_loss.' + loss +
+                            '_optimizer.' + optimizer +
+                            '_patience.' + str(patience) +
+                            '_other_net.' + 'normalized')
 
     os.makedirs(run_path, exist_ok=False)
 
-    # Train model
-    val_metric = model.fit(15000, train_loader, val_loader, patience, run_path)
-    # Clean checkpoint folder from all the checkpoints that are useless
-    clean_folder(run_path, val_metric, delta=0.002)
-
     # Make backup of network and model files into run folder
     shutil.copy('network.py', run_path)
+    shutil.copy('base_networks.py', run_path)
     shutil.copy('model.py', run_path)
     shutil.copy('train.py', run_path)
     shutil.copy('dataset.py', run_path)
     shutil.copy('pytorchtools.py', run_path)
     shutil.copy('DenseNet3D.py', run_path)
     shutil.copy('ResNet.py', run_path)
+
+    # Train model
+    val_metric = model.fit(15000, train_loader, val_loader, patience, run_path)
+    # Clean checkpoint folder from all the checkpoints that are useless
+    clean_folder(run_path, val_metric, delta=0.002)
+
 
 
